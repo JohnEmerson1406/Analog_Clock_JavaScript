61cb32312e4cc733e2152e0743ba0a3504d2ca0e
john-emerson14 <john.carv.sousa@gmail.com>
2020-03-07 16:05:44 -0300

    feat: clock moves every second

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
 
-var data = new Date();
+function executarRelogio() {
+    var data = new Date();
 
-let hr = data.getHours();
-let min = data.getMinutes();
-let seg = data.getSeconds();
-
-
-let posicaoHr = (hr*360/12) + (min*(360/60)/12);
-let posicaoMin = (min*360/60) + (seg*(360/60)/60);
-let posicaoSeg = seg*360/60;
+    let hr = data.getHours();
+    let min = data.getMinutes();
+    let seg = data.getSeconds();
 
 
-PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
-PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
-PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+    let posicaoHr = (hr * 360 / 12) + (min * (360 / 60) / 12);
+    let posicaoMin = (min * 360 / 60) + (seg * (360 / 60) / 60);
+    let posicaoSeg = seg * 360 / 60;
+
+
+    PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
+    PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
+    PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+}
+
+var intervalo = setInterval(executarRelogio, 1000); // 1000 milissegundos = 1 segundo
