e6a662b2a34e98621511a29e385f8c239573e3b9
john-emerson14 <john.carv.sousa@gmail.com>
2020-03-07 15:57:09 -0300

    feat: get current time

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
+
+
+var data = new Date();
+
+let hr = data.getHours();
+let min = data.getMinutes();
+let seg = data.getSeconds();
